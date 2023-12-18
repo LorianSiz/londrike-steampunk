b3d440b553ee90beb1117a3c5144a554c2dc0154
lorian <lorian.sizaire@laposte.net>
2023-12-18 13:51:22 +0100

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const LONDRIKESTEAMPUNK = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+LONDRIKESTEAMPUNK.abilities = {
+  "str": "LONDRIKESTEAMPUNK.AbilityStr",
+  "dex": "LONDRIKESTEAMPUNK.AbilityDex",
+  "con": "LONDRIKESTEAMPUNK.AbilityCon",
+  "int": "LONDRIKESTEAMPUNK.AbilityInt",
+  "wis": "LONDRIKESTEAMPUNK.AbilityWis",
+  "cha": "LONDRIKESTEAMPUNK.AbilityCha"
+};
+
+LONDRIKESTEAMPUNK.abilityAbbreviations = {
+  "str": "LONDRIKESTEAMPUNK.AbilityStrAbbr",
+  "dex": "LONDRIKESTEAMPUNK.AbilityDexAbbr",
+  "con": "LONDRIKESTEAMPUNK.AbilityConAbbr",
+  "int": "LONDRIKESTEAMPUNK.AbilityIntAbbr",
+  "wis": "LONDRIKESTEAMPUNK.AbilityWisAbbr",
+  "cha": "LONDRIKESTEAMPUNK.AbilityChaAbbr"
+};
---
+++ module/helpers/templates.mjs
@@ -0,0 +1,16 @@
+/**
+ * Define a set of template paths to pre-load
+ * Pre-loaded templates are compiled and cached for fast access when rendering
+ * @return {Promise}
+ */
+ export const preloadHandlebarsTemplates = async function() {
+  return loadTemplates([
+
+    // Actor partials.
+    "systems/londrike-steampunk/templates/actor/parts/actor-features.html",
+    "systems/londrike-steampunk/templates/actor/parts/actor-inventory.html",
+    "systems/londrike-steampunk/templates/actor/parts/actor-spells.html",
+    "systems/londrike-steampunk/templates/actor/parts/actor-effects.html",
+      "systems/londrike-steampunk/templates/actor/parts/actor-biography.html",
+  ]);
+};
